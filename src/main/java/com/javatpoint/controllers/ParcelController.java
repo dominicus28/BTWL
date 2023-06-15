a2240e4ae9411d1460381819772ba8660a4a2dec
TheDude <101718545+3p3v@users.noreply.github.com>
2023-06-15 14:24:00 +0200

    CrossOrigin

--- src/main/java/com/javatpoint/controllers/ParcelController.java
+++ src/main/java/com/javatpoint/controllers/ParcelController.java
@@ -18,6 +18,7 @@ import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.http.HttpStatus;
 // import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 // import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -54,6 +55,7 @@ import com.javatpoint.repositories.UserRepository;
 
 
 @RestController
+@CrossOrigin
 public class ParcelController {
     private final MongoTemplate mongoTemplate;
     private final ParcelRepository parcelRepository;
